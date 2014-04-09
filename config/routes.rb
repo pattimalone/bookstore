ea8defd63547d10b01f1f0a25ec621ddf41fb397
Patti_Malone <patinkam@yahoo.com>
2014-04-08 21:40:09 -0400

    lab7

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Bookstore::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :books
-
+  resources :books do
+    get 'page/:page', :action => :index, :on => :collection
+end
+root 'books#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   # Example of regular route:
